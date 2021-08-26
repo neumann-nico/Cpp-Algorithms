51d7dc35c7ef1cd5df2ca137ca6b5988e08690e3
Nico Neumann <nico.neumann@alumni.fh-aachen.de>
2021-08-26 13:28:29 +0200

    remove print statements from BubbleSort

--- src/Sorting/BubbleSort/src/BubbleSort.cpp
+++ src/Sorting/BubbleSort/src/BubbleSort.cpp
@@ -7,12 +7,10 @@ template<typename T>
 void BubbleSort<T>::sort() {
     for (int i = 0; i < this->vec.size(); ++i) {
         for (int j = 0; j < this->vec.size() - i - 1; ++j) {
-            this->printVector();
             if (this->vec[j + 1] < this->vec[j]) {
                 std::swap(this->vec[j], this->vec[j + 1]);
             }
         }
-        std::cout << std::endl;
     }
 }
 
