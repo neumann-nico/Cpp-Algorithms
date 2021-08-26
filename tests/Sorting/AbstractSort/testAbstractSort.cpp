9b4aeca14b8d62bd1aab98735ce69001203636b4
Nico Neumann <nico.neumann@alumni.fh-aachen.de>
2021-08-26 02:02:03 +0200

    add tests for AbstractSort

---
+++ tests/Sorting/AbstractSort/testAbstractSort.cpp
@@ -0,0 +1,24 @@
+#include "gtest/gtest.h"
+#include "AbstractSort/AbstractSort.h"
+
+template <typename T>
+class AbstractSortTest : public AbstractSort<int>{
+public:
+    explicit AbstractSortTest(std::vector<T> &vec) : AbstractSort(vec){};
+    void sort() override {};
+};
+
+
+TEST(AbstractSort, set_get_int) {
+    std::vector<int> vec = {0, 15, 41, 14, 24};
+
+    AbstractSortTest<int> sorter(vec);
+    EXPECT_EQ(sorter.getVector(), vec);
+    EXPECT_FALSE(sorter.isSorted());
+
+    std::vector<int> vec2 = {0, 41, 4, 4, 34};
+    EXPECT_EQ(sorter.getVector(), vec2);
+    EXPECT_FALSE(sorter.isSorted());
+}
+
+template class AbstractSortTest<int>;
